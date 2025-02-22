ff13cda95f208e0a807d10e509fd0a80eccd2b08
Bojan <bdimic@bluecatnetworks.com>
2025-02-22 12:21:53 +0100

    Store uploaded files locally to file system

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
@@ -28,10 +34,79 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Too large file", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid upload file", err)
+		return
+	}
+	defer func(file multipart.File) {
+		err := file.Close()
+		if err != nil {
+			log.Printf("Error closing file: %v\n", err)
+		}
+	}(file)
+	mediaType := header.Header.Get("Content-Type")
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't find video", err)
+		return
+	}
+	if userID != video.UserID {
+		respondWithError(w, http.StatusUnauthorized, "You are not authorized to upload this video", err)
+		return
+	}
+	mediaType, ok := strings.CutPrefix(mediaType, "image/")
+	if !ok {
+		respondWithError(w, http.StatusBadRequest, "Invalid file type", err)
+		return
+	}
+	newVideoPath := filepath.Join(cfg.assetsRoot, fmt.Sprintf("%s.%v", videoID, mediaType))
+	newVideoFile, err := os.Create(newVideoPath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create file", err)
+		return
+	}
+	defer newVideoFile.Close()
+	_, err = io.Copy(newVideoFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't copy file", err)
+		return
+	}
+	dataURL := fmt.Sprintf("http://localhost:%v/assets/%v.%v", cfg.port, videoID, mediaType)
+
+	err = cfg.db.UpdateVideo(database.Video{
+		ID:           videoID,
+		VideoURL:     video.VideoURL,
+		ThumbnailURL: &dataURL,
+		CreateVideoParams: database.CreateVideoParams{
+			Title:       video.Title,
+			Description: video.Description,
+			UserID:      userID,
+		},
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, database.Video{
+		ID:           videoID,
+		VideoURL:     video.VideoURL,
+		ThumbnailURL: &dataURL,
+		CreateVideoParams: database.CreateVideoParams{
+			Title:       video.Title,
+			Description: video.Description,
+			UserID:      userID,
+		},
+	})
 }
